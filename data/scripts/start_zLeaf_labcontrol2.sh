f589ab1924f8bca4b3a3789346ba502f8cd8906f
markuspg <markuspg@users.noreply.github.com>
2016-09-22 19:48:25 +0200

    Changed default 'z-Tree' installation path

--- data/scripts/start_zLeaf_labcontrol2.sh
+++ data/scripts/start_zLeaf_labcontrol2.sh
@@ -2,14 +2,14 @@
 
 # Start a zLeaf with the given arguments
 if [ $# == 2 ]; then
-	DISPLAY=:0.0 /usr/bin/wine /opt/zTree_$1/zleaf.exe /server $2 &
+	DISPLAY=:0.0 /usr/bin/wine /opt/z-Leaves/zTree_$1/zleaf.exe /server $2 &
 	exit
 fi
 if [ $# == 3 ]; then
-	DISPLAY=:0.0 /usr/bin/wine /opt/zTree_$1/zleaf.exe /server $2 /channel $3 &
+	DISPLAY=:0.0 /usr/bin/wine /opt/z-Leaves/zTree_$1/zleaf.exe /server $2 /channel $3 &
 	exit
 fi
 if [ $# == 4 ]; then
-	DISPLAY=:0.0 /usr/bin/wine /opt/zTree_$1/zleaf.exe /server $2 /channel $3 /name $4 &
+	DISPLAY=:0.0 /usr/bin/wine /opt/z-Leaves/zTree_$1/zleaf.exe /server $2 /channel $3 /name $4 &
 	exit
 fi
